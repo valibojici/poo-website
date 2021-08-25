0a7ec09b2c5d60509028ecb75fbadda26d85bed5
Vali Bojici <valibojici@gmail.com>
2021-08-25 12:59:28 +0300

    update

---
+++ public/js/problem.js
@@ -0,0 +1,12 @@
+// function removeScroll(media) {
+//     if (media.matches) { // If media query matches
+//         $('#main-right-col').removeClass("overflow-auto");
+//         console.log('heelo');
+//     } else {
+//         $('#main-right-col').addClass("overflow-auto");
+//     }
+//   }
+//   var media = window.matchMedia("(max-width: 768px)");
+
+//   removeScroll(media) // Call listener function at run time
+//   $(media).on('change', removeScroll);
