3563777d7b4063ff666d529286bf3b7ed3bcb19e
Vali Bojici <valibojici@gmail.com>
2021-08-26 16:38:48 +0300

    udpate

--- public/js/problem.js
+++ public/js/problem.js
@@ -1,6 +1,6 @@
 function changeButtonsSize(media) {
 
-    function addRemoveClass(selector, addClass, removeClass){
+    function addRemoveClass(selector, addClass, removeClass) {
         $(`${selector}`).addClass(addClass);
         $(`${selector}`).removeClass(removeClass);
     }
@@ -12,107 +12,93 @@ function changeButtonsSize(media) {
         addRemoveClass('.next-btn', 'btn-success', 'btn-outline-success');
         addRemoveClass('.prev-btn', 'btn-warning', 'btn-outline-warning');
         addRemoveClass('#copy-btn', 'btn-altlight', 'btn-outline-altlight');
-        addRemoveClass('#correct-btn', 'btn-success','btn-outline-success');
-        addRemoveClass('#incorrect-btn', 'btn-warning','btn-outline-warning');
+        addRemoveClass('#correct-btn', 'btn-success', 'btn-outline-success');
+        addRemoveClass('#incorrect-btn', 'btn-warning', 'btn-outline-warning');
 
 
     } else {
         $('span.btn').removeClass('btn-sm');
 
-        addRemoveClass('.next-btn','btn-outline-success', 'btn-success');
-        addRemoveClass('.prev-btn','btn-outline-warning', 'btn-warning' );
-        addRemoveClass('#copy-btn','btn-outline-altlight', 'btn-altlight');
-        addRemoveClass('#correct-btn','btn-outline-success', 'btn-success');
-        addRemoveClass('#incorrect-btn','btn-outline-warning', 'btn-warning');
+        addRemoveClass('.next-btn', 'btn-outline-success', 'btn-success');
+        addRemoveClass('.prev-btn', 'btn-outline-warning', 'btn-warning');
+        addRemoveClass('#copy-btn', 'btn-outline-altlight', 'btn-altlight');
+        addRemoveClass('#correct-btn', 'btn-outline-success', 'btn-success');
+        addRemoveClass('#incorrect-btn', 'btn-outline-warning', 'btn-warning');
     }
-  }
-  var media = window.matchMedia("(max-width: 992px)");
+}
+var media = window.matchMedia("(max-width: 992px)");
 
-  changeButtonsSize(media) // Call listener function at run time
-  $(media).on('change', ()=>{
-      changeButtonsSize(media);
-  });
+changeButtonsSize(media) // Call listener function at run time
+$(media).on('change', () => {
+    changeButtonsSize(media);
+});
 
-let index = -1;
-let data = null;
+function loadProblem(data) {
+    let code = data.problem;
+    let solution = data.solution;
+    let id = data.id;
 
-async function load(){
-    data = await fetch('https://raw.githubusercontent.com/valibojici/poo-website/main/assets/output.json');
-    data = await data.json();
+    $('#id-problema').text(`${id}`);
+    $("#problem").empty().append(getNumberedCodeBlock(code));
+    $('#solution').html(solution);
 
-    $('.next-btn').on('click', ()=>{
-        let content = data.content;
-        
-        index = (index == content.length-1) ? 0 : index + 1;
-    
-        let code = content[index].problem;
-        let solution = content[index].solution;
-        let id = content[index].id;
-        
-        $('#id-problema').text(`${id}`);
-        $("#problem").empty().append(getNumberedCodeBlock(code));
-        $('#solution').html(decodeHtml(solution));
-     
-        document.querySelectorAll('pre code').forEach(elem => hljs.highlightElement(elem)); 
-        
-        $('#prompt').removeClass('d-none');
-        $('#solution-container').addClass('d-none');
+    $('pre code').get().forEach(elem => hljs.highlightElement(elem));
 
-        $(window).scrollTop($('body').offset().top);
-    });
-
-    $('.prev-btn').on('click', ()=>{
-        // let data = await fetch('https://gist.githubusercontent.com/valibojici/f6806c38994cbbcabe0e3872f0ed15b8/raw/395c4115d6fdaeca10933382868a215301929c84/test2.cpp');
-       
-        let content = data.content;
-
-        index = (index == 0) ? content.length - 1 : index-1;
-    
-        let code = content[index].problem;
-        let solution = content[index].solution;
-        let id = content[index].id;
-        
-        $('#id-problema').text(`${id}`);
-    
-        $("#problem").empty().append(getNumberedCodeBlock(code));
-        $('#solution').html(decodeHtml(solution));
-        
-        document.querySelectorAll('pre code').forEach(elem => hljs.highlightElement(elem));   
-        
-        $('#prompt').removeClass('d-none');
-        $('#solution-container').addClass('d-none');
-
-        $(window).scrollTop($('body').offset().top);
-    });
-    
-    $('.next-btn').first().trigger('click');
-
-    $('#correct-btn, #incorrect-btn').on('click', event=>{
-        $('#prompt').animate({ opacity : 0,}, 200, ()=>{
-            $("#prompt").addClass('d-none');            
-            $("#prompt").css({opacity : 100});
-            $('#solution-container').removeClass('d-none');
-            
-            $(window).scrollTop($('#solution-container').offset().top);
-        });
-        
-    })
+    $('#prompt').removeClass('d-none');
+    $('#solution-container').addClass('d-none');
 }
 
-load();
+async function loadData() {
+    let data = await fetch('https://raw.githubusercontent.com/valibojici/poo-website/main/assets/output.json');
+    data = await data.json();
+    data = data.content;
+
+    let index = 0;
+
+    $('.next-btn, .prev-btn').on('click', (event) => {
+        let $buttonPressed = $(event.target);
+        if ($buttonPressed.hasClass('next-btn')) {
+            index = (index == data.length - 1) ? 0 : index + 1;
+        } else {
+            index = (index <= 0) ? data.length - 1 : index - 1;
+        }
+
+        loadProblem(data[index]);
+
+        $(window).scrollTop($('body').offset().top);
+    });
+
+    $('#correct-btn, #incorrect-btn').on('click', event => {
+        $('#prompt').animate({
+            opacity: 0,
+        }, 200, () => {
+            $("#prompt").addClass('d-none');
+            $("#prompt").css({
+                opacity: 100
+            });
+            $('#solution-container').removeClass('d-none');
+
+            $(window).scrollTop($('#solution-container').offset().top);
+        });
+    });
+
+    loadProblem(data[0]);
+}
+
+loadData();
 
 
-$('#copy-btn').on('click', e =>{
+$('#copy-btn').on('click', e => {
     copyToClipboard(document.querySelectorAll("#problem code"));
     let originalText = $("#copy-btn").html();
     $("#copy-btn").html('Text copiat!');
-    setTimeout(()=>{
+    setTimeout(() => {
         $("#copy-btn").html(originalText);
     }, 1500);
 });
 
-function copyToClipboard (elements) {
-    if(!elements.length){
+function copyToClipboard(elements) {
+    if (!elements.length) {
         elements = [elements];
     }
     // Create a new textarea element and give it id='temp_element'
@@ -123,7 +109,7 @@ function copyToClipboard (elements) {
     // Now append it to your page somewhere, I chose <body>
     document.body.appendChild(textarea)
     // Give our textarea a value of whatever inside the elements
-    for(let elem of elements){
+    for (let elem of elements) {
         textarea.value += (elem.innerText.trim()) ? elem.innerText + '\n' : '\n';
     }
     textarea.value = textarea.value.trim();
@@ -133,16 +119,16 @@ function copyToClipboard (elements) {
     document.execCommand('copy')
     // Remove the textarea
     document.body.removeChild(textarea)
-  }
+}
 
 
-  function getNumberedCodeBlock(code){
+function getNumberedCodeBlock(code) {
     let div = document.createElement('div');
     div.id = 'lines-container';
 
     let count = 0;
-    for(let line of code.split('\n')){
-        
+    for (let line of code.split('\n')) {
+
         count++;
         let span = document.createElement('span');
         let number = document.createElement('span');
@@ -150,10 +136,10 @@ function copyToClipboard (elements) {
         number.classList.add('noselect');
         number.classList.add('line-no');
 
-        
+
         let pre = document.createElement('pre');
         pre.classList.add('line-code')
-        
+
         let code = document.createElement('code');
         code.classList.add('language-cpp');
 
@@ -170,9 +156,9 @@ function copyToClipboard (elements) {
     return div;
 }
 
-  
-  function decodeHtml(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-}
+
+// function decodeHtml(html) {
+//     var txt = document.createElement("textarea");
+//     txt.innerHTML = html;
+//     return txt.value;
+// }
