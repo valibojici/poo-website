bc3382d5fbac14f0445134fde4fb0d07c6f0ceee
Vali Bojici <valibojici@gmail.com>
2021-08-28 12:59:13 +0300

    update

--- public/js/tips.js
+++ public/js/tips.js
@@ -1,3 +1,15 @@
 $('body').css({'padding-top': parseFloat($('#navbar-container').css('height'))});
 
-$("body").scrollspy({ target: ".scrollspy .scrollspy2" });
+let mq = window.matchMedia('(max-width: 768px)');
+
+removeLeadParagraphsOnMobile(mq);
+
+$(mq).on('change', ()=>{removeLeadParagraphsOnMobile(mq)});
+
+function removeLeadParagraphsOnMobile(media){
+    if(media.matches){
+        $(".article-content ").removeClass('lead');
+    } else {
+        $(".article-content ").addClass('lead');
+    }
+}
