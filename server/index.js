85b4f4f24237e36a226e1dcd3274f4dbda910577
Vali Bojici <valibojici@gmail.com>
2021-09-05 14:47:07 +0300

    update

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const data = require('./output.json');
+const app = express();
+const port = 3000;
+
+app.use(cors({
+	origin: 'http://127.0.0.1:5500'
+}))
+
+app.get('/get', (req, res) => {
+  res.header("Content-Type",'application/json');
+  res.send(JSON.parse(fs.readFileSync('output.json')));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
